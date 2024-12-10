7e12eb5e46c96e6e66da82baa5b44ae298e7bbdd
k-jaehyun <wogus146@gmail.com>
2024-12-10 18:04:30 +0900

    🩹 fix: ProductRepository 오타 수정

--- product-service/src/main/java/com/logistics/platform/product_service/domain/repository/ProductRepository.java
+++ product-service/src/main/java/com/logistics/platform/product_service/domain/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.logistics.platform.product_service.domain.repository;
 
 import com.logistics.platform.product_service.domain.model.Product;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 
 }
